25acc918189da1cff35755a110a924013c0dccb6
Egor Lysenko <uprendis@gmail.com>
2020-09-10 19:36:30 +0700

    enable truffle testing

--- truffle-config.js
+++ truffle-config.js
@@ -11,15 +11,15 @@ module.exports = {
         }
     },
     networks: {
-        development: {
-            host: "operavm",
-            port: 7545,
-            network_id: "4002"
-        },
-        test: {
-            host: "wsapi.testnet.fantom.network",
-            port: 80,
-            network_id: "4002"
-        }
+        // development: {
+        //     host: "operavm",
+        //     port: 7545,
+        //     network_id: "4002"
+        // },
+        // test: {
+        //     host: "wsapi.testnet.fantom.network",
+        //     port: 80,
+        //     network_id: "4002"
+        // }
     }
 };
